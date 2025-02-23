22c9a6ef40e79f50a219b8609de8f16680f87675
Hüsü <73742151+husudu@users.noreply.github.com>
2025-02-24 03:56:12 +0400

    Afk botlog xətası

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -278,7 +278,7 @@ async def type_afk_is_not_true(notafk):
             for i in USERS:
                 name = await notafk.client.get_entity(i)
                 name0 = "name.first_name"
-                await notafk.client.send_message(BOTLOG_CHATID,"[{name0}](tg://user?id=i) sizə {USERS[i]} mesaj göndərdi")
+                await notafk.client.send_message(BOTLOG_CHATID, f"[{name0}](tg://user?id=i) sizə {USERS[i]} mesaj göndərdi")
         COUNT_MSG = 0
         USERS = {}
         AFKREASON = None
